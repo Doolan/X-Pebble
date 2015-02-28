26a1828e0d4859090c04a9961822f4819cd7776b
Doolan <doolansr@rose-hulman.edu>
2015-02-28 01:06:58 -0500

    basic layout

---
+++ profile.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="main.css">
+	<script src="../libraries/jquery-2.1.3.min.js"></script>
+
+	<link href="images/fav.gif" type="image/gif" rel="shortcut icon"/>
+</head>
+<body>
+	<?PHP include 'navbar.html'; ?>
+
+</body>
+</html>
