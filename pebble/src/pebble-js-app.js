f2990ff4d191436ff8495aed128e324660bc5d86
Steven Doolan <doolansr@rose-hulman.edu>
2015-03-01 00:42:24 -0500

    server access

--- pebble/src/pebble-js-app.js
+++ pebble/src/pebble-js-app.js
@@ -26,39 +26,30 @@ var xhrRequest = function(url, type, callback){
 //TOPEBBLE: Step 1 get request
 function getWorkout(){
     console.log('hit get workout');
-    /**
+    
     //TOPEBBLE: Step 1 get request
     var url = "http://x-pebble.azurewebsites.net/scripts/get_all_exercise_ids.php?pebbleid=qwerty";    
     //send request to Josh for the data
     xhrRequest(url, 'GET', function(responseText){
-        var exerciseString = responseText;
-        console.log(exerciseString);
-     **/
+        var exerciseString = JSON.parse(responseText);
+        console.log(responseText, exerciseString);
+     
    // var array = new Uint8Array(100);
-    var array1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
-    /**
-    var arraytyped = new Uint8Array(array1.length);
-    for(var i =0; i<array1.length; i++){
-        arraytyped[i]=array1[i];
-    }
-    **/
-    //TOPEBBLE: Step 2, build Dictionary (required)
+    // array1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+    
+        //TOPEBBLE: Step 2, build Dictionary (required)
         var mydictionary = {
-           "KEY_WORKOUTPLAN": array1 //exerciseString,
-            //"KEY_WORKOUTSIZE": 7
+           "KEY_WORKOUTPLAN": exerciseString
         };
-       // dict_write_data(mydictionary, KEY_WORKOUTPLAN, 
-    //TOPEBBLE: Send to Pebble 
+        //TOPEBBLE: Send to Pebble 
         Pebble.sendAppMessage(mydictionary,
               function(e){
                  console.log("send stuff to pebble -- success");
               },
               function(e){
                   console.log("error-ed on send");
-              }
-          );
-        
-    //}
+              });
+       });
 }
 
 
